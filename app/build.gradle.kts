083658357037d81f6c6b2a00a6691879e91ef33b
Monitor Dev <dev@monitor.local>
2026-05-07 17:56:11 +0800

    fix: move signingConfigs before buildTypes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,20 +22,20 @@ android {
         )
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-
     signingConfigs {
         create("release") {
             storeFile = rootProject.file("monitor-release.jks")
             storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "monitor2605"
             keyAlias = "monitor"
             keyPassword = System.getenv("KEY_PASSWORD") ?: "monitor2605"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
